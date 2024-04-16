c4f08b0512ce9353a6edc8514c1433f3d4249d39
Sarasaenz12 <158489143+Sarasaenz12@users.noreply.github.com>
2024-04-16 09:07:43 -0500

    Update funcioneseje1.js

--- funcioneseje1.js
+++ funcioneseje1.js
@@ -3,39 +3,39 @@ const denominations = [50000, 20000, 10000, 5000, 2000, 1000];
 // Función para obtener la cantidad de dinero que el usuario desea retirar.
 function getWithdrawalAmount() {
   const userInput = prompt("¿Cuánto dinero deseas retirar?");
-  return parseFloat(userInput); // Convierte la entrada del usuario en un número decimal.
+  return parseFloat(userInput); 
 }
 
 // Función para calcular la cantidad de billetes necesarios para el retiro.
 function calculateBilletsNeeded(amount) {
   return denominations.map(denomination => { // Mapea sobre cada denominación de billete.
-    const billetsNeeded = Math.floor(amount / denomination); // Calcula la cantidad de billetes necesarios para la denominación actual.
-    amount -= billetsNeeded * denomination; // Actualiza el monto restando el valor de los billetes necesarios.
-    return billetsNeeded; // Devuelve la cantidad de billetes necesarios para la denominación actual.
+    const billetsNeeded = Math.floor(amount / denomination); 
+    amount -= billetsNeeded * denomination;
+    return billetsNeeded; 
   });
 }
 
 // Función para verificar si el monto de retiro es válido.
 function isValidWithdrawalAmount(amount) {
-  return amount > 0 && amount % 1 === 0; // Retorna verdadero si el monto es positivo y es un número entero.
+  return amount > 0 && amount % 1 === 0;
 }
 
 // Función que realiza la transacción en el cajero automático.
 function ATMTransaction() {
-  const withdrawalAmount = getWithdrawalAmount(); // Obtiene el monto de retiro ingresado por el usuario.
+  const withdrawalAmount = getWithdrawalAmount();
 
-  if (!isValidWithdrawalAmount(withdrawalAmount)) { // Verifica si el monto de retiro es válido.
-    alert("El monto solicitado no es válido."); // Muestra un mensaje de alerta si el monto no es válido.
+  if (!isValidWithdrawalAmount(withdrawalAmount)) { 
+    alert("El monto solicitado no es válido.");
     return; // Termina la función.
   }
 
   const billetsNeeded = calculateBilletsNeeded(withdrawalAmount); // Calcula la cantidad de billetes necesarios para el retiro.
 
-  alert("Para retirar $" + withdrawalAmount + ", necesitas:"); // Muestra un mensaje con el monto de retiro.
+  alert("Para retirar $" + withdrawalAmount + ", necesitas:");
 
   denominations.forEach((denomination, index) => { // Itera sobre cada denominación de billete.
-    if (billetsNeeded[index] > 0) { // Verifica si se necesitan billetes para la denominación actual.
-      alert("- " + billetsNeeded[index] + " billetes de $" + denomination); // Muestra la cantidad de billetes necesarios para la denominación actual.
+    if (billetsNeeded[index] > 0) {
+      alert("- " + billetsNeeded[index] + " billetes de $" + denomination);
     }
   });
 }
